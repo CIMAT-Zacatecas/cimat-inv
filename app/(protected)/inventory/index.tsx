d38c2f4cd3681f316a7d9313ef1e5f6e45a9400d
Roberto Villegas <hola@robertovillegas.com>
2024-11-14 16:13:42 -0600

    feat: style search

--- app/(protected)/inventory/index.tsx
+++ app/(protected)/inventory/index.tsx
@@ -24,6 +24,7 @@ import {
 } from "@/components/ui/select";
 import { VStack } from "@/components/ui/vstack";
 import { HStack } from "@/components/ui/hstack";
+import { Card } from "@/components/ui/card";
 
 export default function Inventory() {
   const { data: bienes = [], isLoading, error, refetch } = useInventory();
@@ -64,105 +65,107 @@ export default function Inventory() {
   }
 
   return (
-    <Container>
-      <VStack space="md" className="mb-4">
-        <Input>
-          <InputField
-            placeholder="Buscar por ID o descripción..."
-            onChangeText={debouncedSetSearch}
-          />
-          <InputIcon as={Search} />
-        </Input>
+    <Container removeVerticalPadding>
+      <Card className="my-4">
+        <VStack space="md">
+          <Input>
+            <InputField
+              placeholder="Buscar por ID o descripción..."
+              onChangeText={debouncedSetSearch}
+            />
+            <InputIcon as={Search} />
+          </Input>
 
-        <HStack space="sm">
-          <Select
-            selectedValue={filters.categoryId?.toString()}
-            onValueChange={(value) =>
-              setFilters((prev) => ({
-                ...prev,
-                categoryId: value ? Number(value) : undefined,
-              }))
-            }>
-            <SelectTrigger>
-              <SelectInput placeholder="Categoría" />
-            </SelectTrigger>
-            <SelectPortal>
-              <SelectBackdrop />
-              <SelectContent>
-                <SelectDragIndicatorWrapper>
-                  <SelectDragIndicator />
-                </SelectDragIndicatorWrapper>
-                <SelectItem label="Todas" value="" />
-                {categoryOptions.map((option) => (
-                  <SelectItem
-                    key={option.value}
-                    label={option.label}
-                    value={option.value.toString()}
-                  />
-                ))}
-              </SelectContent>
-            </SelectPortal>
-          </Select>
+          <HStack space="sm">
+            <Select
+              selectedValue={filters.categoryId?.toString()}
+              onValueChange={(value) =>
+                setFilters((prev) => ({
+                  ...prev,
+                  categoryId: value ? Number(value) : undefined,
+                }))
+              }>
+              <SelectTrigger>
+                <SelectInput placeholder="Categoría" />
+              </SelectTrigger>
+              <SelectPortal>
+                <SelectBackdrop />
+                <SelectContent>
+                  <SelectDragIndicatorWrapper>
+                    <SelectDragIndicator />
+                  </SelectDragIndicatorWrapper>
+                  <SelectItem label="Todas" value="" />
+                  {categoryOptions.map((option) => (
+                    <SelectItem
+                      key={option.value}
+                      label={option.label}
+                      value={option.value.toString()}
+                    />
+                  ))}
+                </SelectContent>
+              </SelectPortal>
+            </Select>
 
-          <Select
-            selectedValue={filters.statusId?.toString()}
-            onValueChange={(value) =>
-              setFilters((prev) => ({
-                ...prev,
-                statusId: value ? Number(value) : undefined,
-              }))
-            }>
-            <SelectTrigger>
-              <SelectInput placeholder="Estado" />
-            </SelectTrigger>
-            <SelectPortal>
-              <SelectBackdrop />
-              <SelectContent>
-                <SelectDragIndicatorWrapper>
-                  <SelectDragIndicator />
-                </SelectDragIndicatorWrapper>
-                <SelectItem label="Todos" value="" />
-                {statusOptions.map((option) => (
-                  <SelectItem
-                    key={option.value}
-                    label={option.label}
-                    value={option.value.toString()}
-                  />
-                ))}
-              </SelectContent>
-            </SelectPortal>
-          </Select>
+            <Select
+              selectedValue={filters.statusId?.toString()}
+              onValueChange={(value) =>
+                setFilters((prev) => ({
+                  ...prev,
+                  statusId: value ? Number(value) : undefined,
+                }))
+              }>
+              <SelectTrigger>
+                <SelectInput placeholder="Estado" />
+              </SelectTrigger>
+              <SelectPortal>
+                <SelectBackdrop />
+                <SelectContent>
+                  <SelectDragIndicatorWrapper>
+                    <SelectDragIndicator />
+                  </SelectDragIndicatorWrapper>
+                  <SelectItem label="Todos" value="" />
+                  {statusOptions.map((option) => (
+                    <SelectItem
+                      key={option.value}
+                      label={option.label}
+                      value={option.value.toString()}
+                    />
+                  ))}
+                </SelectContent>
+              </SelectPortal>
+            </Select>
 
-          <Select
-            selectedValue={filters.locationId?.toString()}
-            onValueChange={(value) =>
-              setFilters((prev) => ({
-                ...prev,
-                locationId: value ? Number(value) : undefined,
-              }))
-            }>
-            <SelectTrigger>
-              <SelectInput placeholder="Ubicación" />
-            </SelectTrigger>
-            <SelectPortal>
-              <SelectBackdrop />
-              <SelectContent>
-                <SelectDragIndicatorWrapper>
-                  <SelectDragIndicator />
-                </SelectDragIndicatorWrapper>
-                <SelectItem label="Todas" value="" />
-                {locationOptions.map((option) => (
-                  <SelectItem
-                    key={option.value}
-                    label={option.label}
-                    value={option.value.toString()}
-                  />
-                ))}
-              </SelectContent>
-            </SelectPortal>
-          </Select>
-        </HStack>
-      </VStack>
+            <Select
+              selectedValue={filters.locationId?.toString()}
+              onValueChange={(value) =>
+                setFilters((prev) => ({
+                  ...prev,
+                  locationId: value ? Number(value) : undefined,
+                }))
+              }>
+              <SelectTrigger>
+                <SelectInput placeholder="Ubicación" />
+              </SelectTrigger>
+              <SelectPortal>
+                <SelectBackdrop />
+                <SelectContent>
+                  <SelectDragIndicatorWrapper>
+                    <SelectDragIndicator />
+                  </SelectDragIndicatorWrapper>
+                  <SelectItem label="Todas" value="" />
+                  {locationOptions.map((option) => (
+                    <SelectItem
+                      key={option.value}
+                      label={option.label}
+                      value={option.value.toString()}
+                    />
+                  ))}
+                </SelectContent>
+              </SelectPortal>
+            </Select>
+          </HStack>
+        </VStack>
+      </Card>
 
       {filteredItems.length === 0 ? (
         <Container centered>
@@ -170,7 +173,6 @@ export default function Inventory() {
         </Container>
       ) : (
         <FlashList
-          ListHeaderComponent={() => <Separator height={8} />}
           estimatedItemSize={50}
           data={filteredItems}
           renderItem={({ item }) => (
